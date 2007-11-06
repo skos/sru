319710b12459f7231e32251a1713123bce12bd86
hrynek <hrynek@e0d11117-6615-0410-a283-869df193e4dd>
2007-11-06 20:14:33 +0000

    ladniejszy sposob na okreslenie dat waznosci komputerow
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@39 e0d11117-6615-0410-a283-869df193e4dd

--- app/conf/sru.php
+++ app/conf/sru.php
@@ -3,11 +3,6 @@
 class UFconf_Sru
 extends UFconf {
 
-	protected $computerAvailableTo;
-	protected $computerAvailableMaxTo;
-
-	public function __construct() {
-		$this->computerAvailableTo = NOW + 3600*24*7;
-		$this->computerAvailableMaxTo = NOW + 3600*24*100;
-	}
+	protected $computerAvailableTo = '+ 30 days';
+	protected $computerAvailableMaxTo = '2008-07-01';
 }
