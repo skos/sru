37012a06eb2edffd338b827b04a18a084a140ae0
hrynek <hrynek@e0d11117-6615-0410-a283-869df193e4dd>
2007-11-15 16:20:45 +0000

    poprawiona pozycja <pre/> w logach i link w naglowku
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@49 e0d11117-6615-0410-a283-869df193e4dd

--- app/tpl/indexadmin.php
+++ app/tpl/indexadmin.php
@@ -17,7 +17,7 @@ extends UFtpl {
 <body>
 <div id="body">
 <div id="head">
-<h1><a href="<?=UFURL_BASE;?>"><abbr title="Sieć Komputerowa Osiedla Studenckiego">SKOS</abbr></a></h1>
+<h1><a href="<?=UFURL_BASE;?>/"><abbr title="Sieć Komputerowa Osiedla Studenckiego">SKOS</abbr></a></h1>
 </div><!-- head -->
 <?=$d['menuAdmin'];?>
 <div id="main">
@@ -27,15 +27,13 @@ extends UFtpl {
 <a href="mailto:hrynek@hrynek.com">Maciej "HryneK" Hryniewicz</a>
 </div><!-- foot -->
 </div><!-- body -->
-<pre>
 <?
 $stop = microtime(true);
 $start = $this->_srv->get('msg')->get('timeStart');
 $boot = $this->_srv->get('msg')->get('timeBoot');
 echo '<!-- boot: '.number_format(1000*($boot-$start), 1).'ms, total: '.number_format(1000*($stop-$start), 1).'ms -->';
-echo print_r(UFra::logs(), true);
+echo '<pre>'.print_r(UFra::logs(), true).'</pre>';
 ?>
-</pre>
 </body>
 </html><?
 	}
