abbd2cde77b220c4af6d7e215f611c2e036671e5
hrynek <hrynek@e0d11117-6615-0410-a283-869df193e4dd>
2008-10-12 17:51:44 +0000

    dodane ip-ki 153.19.*.0
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@154 e0d11117-6615-0410-a283-869df193e4dd

---
+++ app/sql/patch/r153.sql
@@ -0,0 +1,14 @@
+insert into ipv4s (ip, dormitory_id) values ('153.19.209.0', 1);
+insert into ipv4s (ip, dormitory_id) values ('153.19.210.0', 2);
+insert into ipv4s (ip, dormitory_id) values ('153.19.211.0', 2);
+insert into ipv4s (ip, dormitory_id) values ('153.19.212.0', 2);
+insert into ipv4s (ip, dormitory_id) values ('153.19.213.0', 3);
+insert into ipv4s (ip, dormitory_id) values ('153.19.214.0', 4);
+insert into ipv4s (ip, dormitory_id) values ('153.19.215.0', 5);
+insert into ipv4s (ip, dormitory_id) values ('153.19.216.0', 7);
+insert into ipv4s (ip, dormitory_id) values ('153.19.217.0', 8);
+insert into ipv4s (ip, dormitory_id) values ('153.19.218.0', 9);
+insert into ipv4s (ip, dormitory_id) values ('153.19.219.0', 9);
+insert into ipv4s (ip, dormitory_id) values ('153.19.220.0', 10);
+insert into ipv4s (ip, dormitory_id) values ('153.19.221.0', 11);
+insert into ipv4s (ip, dormitory_id) values ('153.19.222.0', 12);
