8e29d432f6aaa28c625b89acffc34ec8250f3897
hrynek <hrynek@e0d11117-6615-0410-a283-869df193e4dd>
2008-10-16 23:33:22 +0000

    skrypt pobierajacy inc-e do dhcp
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@176 e0d11117-6615-0410-a283-869df193e4dd

---
+++ app/bin/dhcp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+tmp="/tmp/sru-api.tmp"
+dir='/opt/skos/dhcp'
+
+wget -q -O $tmp http://sru.ds.pg.gda.pl/api/dhcp/stud && mv $tmp "$dir/studs.inc"
+wget -q -O $tmp http://sru.ds.pg.gda.pl/api/dhcp/org && mv $tmp "$dir/org.inc"
+wget -q -O $tmp http://sru.ds.pg.gda.pl/api/dhcp/adm && mv $tmp "$dir/adm.inc"
+/etc/init.d/dhcpd restart
