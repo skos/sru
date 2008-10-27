b063612c6822f35ff85c72b64fe047883aec736c
hrynek <hrynek@e0d11117-6615-0410-a283-869df193e4dd>
2008-10-27 23:06:32 +0000

    poprawiona nazwa bazy
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@188 e0d11117-6615-0410-a283-869df193e4dd

--- app/bin/walet.sh
+++ app/bin/walet.sh
@@ -3,7 +3,7 @@ dir='/opt/skos/sru-walet'
 tmp="/tmp/sru-walet.tmp"
 changed="$(dirname $0)/changed.sh"
 walet="$(dirname $0)/walet2sql.py"
-db='skos'
+db='sru'
 
 $walet > $tmp && mv $tmp "$dir/walet.sql" && \
 $changed "$dir/walet.sql" && psql "$db" -f "$dir/walet.sql" -q
