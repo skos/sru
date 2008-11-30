ad949742bed1e12bfe0cb6ac655fdce3a6c78fa9
hrynek <hrynek@e0d11117-6615-0410-a283-869df193e4dd>
2008-11-30 20:28:56 +0000

    poprawka nazw pol
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@228 e0d11117-6615-0410-a283-869df193e4dd

--- app/map/sru/dormitory/get.php
+++ app/map/sru/dormitory/get.php
@@ -6,22 +6,22 @@ class UFmap_Sru_Dormitory_Get
 extends UFmap {
 
 	protected $columns = array(
-		'id'             => 'id',
-		'name'           => 'name',
-		'alias'          => 'alias',
-		'userCount'     => 'users_count',
+		'id'           => 'id',
+		'name'         => 'name',
+		'alias'        => 'alias',
+		'userCount'    => 'users_count',
 		'computerCount' => 'computers_count',
-		'usersMax'      => 'users_max',
-		'computersMax'  => 'computers_max',
+		'usersMax'     => 'users_max',
+		'computersMax' => 'computers_max',
 	);
 	protected $columnTypes = array(
-		'id'             => self::INT,
-		'name'           => self::TEXT,
-		'alias'          => self::TEXT,
-		'userCount'     => self::INT,
+		'id'           => self::INT,
+		'name'         => self::TEXT,
+		'alias'        => self::TEXT,
+		'userCount'    => self::INT,
 		'computerCount' => self::INT,
-		'users_max'     => self::INT,
-		'computers_max' => self::INT,
+		'usersMax'     => self::INT,
+		'computersMax' => self::INT,
 	);
 	protected $tables = array(
 		'' => 'dormitories',
