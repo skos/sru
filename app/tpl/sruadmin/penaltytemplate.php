bf7fee9a08feba3cf1e09ebe465138587e8f4598
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2009-09-06 10:02:12 +0000

    Wyswietlanie w szablonach kary takze informacji o minimalnej dlugosci kary
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@334 e0d11117-6615-0410-a283-869df193e4dd

--- app/tpl/sruadmin/penaltytemplate.php
+++ app/tpl/sruadmin/penaltytemplate.php
@@ -8,7 +8,7 @@ extends UFtpl_Common {
 	public function choose(array $d) {
 		$url = $this->url();
 		foreach ($d as $t) {
-			echo '<li><h3><a href="'.$url.'/template:'.$t['id'].'">'.$t['title'].'</a></h3><p>'.$t['description'].' <em>('.$t['duration'].' dni)</em></p></li>';
+			echo '<li><h3><a href="'.$url.'/template:'.$t['id'].'">'.$t['title'].'</a></h3><p>'.$t['description'].' <em>('.$t['duration'].' dni / '.$t['amnesty'].' dni)</em></p></li>';
 		}
 		echo '<li><h3><a href="'.$this->url().'/template:0">Inne</a></h3><p>Żadne z powyższych</p></li>';
 	}
