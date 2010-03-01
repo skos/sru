6091c10808854f9f25888e0228f010cf597fc854
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-03-01 10:43:04 +0000

    ktualizacja configa i skryptu DNS do najnowszej wersji - commit porzadkujący
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@405 e0d11117-6615-0410-a283-869df193e4dd

--- app/bin/dns.sh
+++ app/bin/dns.sh
@@ -7,14 +7,15 @@ dir="$base/inc"
 changed=0
 
 wget -q --no-check-certificate -O $tmp https://sru.ds.pg.gda.pl/api/dns/ds && mv $tmp "$dir/ds.inc"
-$increment "$base/M/ds.zone" "$dir/ds.inc" && changed=1
+$increment "$base/M/pl.gda.pg.ds" "$dir/ds.inc" && changed=1
 wget -q --no-check-certificate -O $tmp https://sru.ds.pg.gda.pl/api/dns/adm && mv $tmp "$dir/adm.inc"
-$increment "$base/M/adm.zone" "$dir/adm.inc" && changed=1
+$increment "$base/M/pl.gda.pg.ds.adm" "$dir/adm.inc" && changed=1
 for i in `seq 207 223`; do
 	wget -q --no-check-certificate -O $tmp "https://sru.ds.pg.gda.pl/api/dns/${i}" && mv $tmp "$dir/${i}.inc"
-	$increment "$base/M/${i}.ds" "$dir/${i}.inc" && changed=1
+	$increment "$base/M/153.19.${i}" "$dir/${i}.inc" && changed=1
 done
 if [[ $changed == "1" ]]; then
 	echo -n "$(date) "
-	/usr/sbin/rndc reload
+	#/usr/sbin/rndc reload
+	/etc/init.d/bind9 restart
 fi
--- app/conf/sru.php
+++ app/conf/sru.php
@@ -4,14 +4,14 @@ class UFconf_Sru
 extends UFconf {
 
 	#protected $computerAvailableTo = '+ 30 days';
-	protected $computerAvailableTo = '2009-10-15';	// data waznosci noworejestrowanych komputerow
-	protected $computerAvailableMaxTo = '2009-10-15';	// data na przycisku dostepnym administratorom
+	protected $computerAvailableTo = '2010-07-01';	// data waznosci noworejestrowanych komputerow
+	protected $computerAvailableMaxTo = '2010-07-01';	// data na przycisku dostepnym administratorom
 
 	protected $noEthers = array(
 		'153.19.208.22',
 	);
 
-	protected $checkWalet = true;	// sprawdzac dane uzytkownikow z baza osiedla?
+	protected $checkWalet = false;	// sprawdzac dane uzytkownikow z baza osiedla?
 	protected $sendEmail = true;	// wysylac maile dot. kar, edycji danych i danych kompow?
 	protected $emailPrefix = '[SRU]';	// prefix maili wysyłanych ze SRU
 	protected $exclusions = array('ADMINISTRACJA', 'SKOS', 'Samorząd Studentów', 'Studencka Agencja');	// wykluczenia nazw (imion) ze zliczeń etc.
