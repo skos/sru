def215b9668eefbcd46252177b61887610f22006
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-03-15 20:55:16 +0000

    #118, #119 - nie wrzuciły się wszystkie nowe pliki :(
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@418 e0d11117-6615-0410-a283-869df193e4dd

---
+++ app/acl/sruadmin/service.php
@@ -0,0 +1,15 @@
+<?
+/**
+ * sprawdzanie uprawnien
+ */
+class UFacl_SruAdmin_Service
+extends UFlib_ClassWithService {
+	
+	protected function _loggedIn() {
+		return $this->_srv->get('session')->is('auth');
+	}
+
+	public function edit() {
+		return $this->_loggedIn();
+	}
+}
