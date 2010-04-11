abb2a3d7df6ee4b599fadcf11fa5b755c4aef532
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-04-11 12:07:57 +0000

    #207 - poprawki do sortowania pokoi
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@434 e0d11117-6615-0410-a283-869df193e4dd

--- app/tpl/sruadmin/room.php
+++ app/tpl/sruadmin/room.php
@@ -8,27 +8,41 @@ extends UFtpl_Common {
 	public function listRooms(array $d) {
 		$url = $this->url(0).'/dormitories/';
 		
-		$lastRoom = '0';
+		$lastRoom = -1;
 		
 		$dorm = isset($d[0]['dormitoryAlias']) ? $d[0]['dormitoryAlias'] : '';
 		$aliases = array();
-		$i = 0;
+		
 		foreach ($d as $c) {
-			$aliases[$i] = $c['alias'];
-			$i++;
+			$roomInt = (int)$c['alias'];
+			if (!array_key_exists($roomInt, $aliases)) {
+				$aliases[$roomInt] = new Connector();
+			}
+			$aliases[$roomInt]->addRoom($c['alias']);
 		}
-		sort($aliases, SORT_NUMERIC);
+		ksort($aliases);
 
-		foreach ($aliases as $c) {
-			if(($lastRoom[0] != $c[0] && ((int)$c < 1 || (int)$c > 9)) || (int)$lastRoom == 0 || ((int)$lastRoom < 100 && (int)$c >= 100)) {
-				if($lastRoom != '0') {
+		while ($alias = current($aliases)) {
+			$alias->sort();
+			$connector = key($aliases);
+			if((substr($lastRoom, 0, 1) != substr($connector, 0,1) && ($connector < 1 || $connector > 9)) || $lastRoom == 0 || ($lastRoom < 100 && $connector >= 100)) {
+				if($lastRoom != -1) {
 					echo '</ul><ul>';
 				} else {
 					echo '<ul class="first">';
 				}
 			}
-			echo '<li><a href="'.$url.$dorm.'/'.$c.'">'.$c.'</a></li>';
-			$lastRoom = $c;
+			$rooms = $alias->getRooms();
+			if ($rooms == null || count($rooms) == 0) {
+				echo '<li><a href="'.$url.$dorm.'/'.$connector.'">'.$connector.'</a></li>';
+			} else {
+				foreach ($rooms as $room) {
+					$dispRoom = $connector == 0 ? $room : $connector.$room;
+					echo '<li><a href="'.$url.$dorm.'/'.$dispRoom.'">'.$dispRoom.'</a></li>';
+				}
+			}
+			$lastRoom = $connector;
+			next($aliases);
 		}
 		echo '</ul>';
 	}
@@ -56,6 +70,28 @@ extends UFtpl_Common {
 		echo $form->comment('', array('type'=>$form->TEXTAREA, 'rows'=>5));
 		echo $form->_submit('Zapisz');
 		echo $form->_end();
-		echo $form->_end(true);		
-	}		
+		echo $form->_end(true);
+	}
+}
+
+class Connector
+{
+	private $rooms = array();
+
+	public function addRoom($room) {
+		$roomInt = (int)$room;
+		if ($roomInt == 0) {
+			$this->rooms[] = $room;
+		} else if (strlen($roomInt) < strlen($room)) {
+			$this->rooms[] = substr($room, strlen($roomInt));
+		}
+	}
+	
+	public function sort() {
+		sort($this->rooms);
+	}
+
+	public function getRooms() {
+		return $this->rooms;
+	}
 }
