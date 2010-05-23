0c44d6a53f5f31a243edefacb1730ba5d836f036
eshelion <eshelion@e0d11117-6615-0410-a283-869df193e4dd>
2010-05-23 21:31:49 +0000

    #190 Usunięcie "dla" i zmniejszenie mniej istotnych części informacji w sekcjach dotyczących usług.
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@471 e0d11117-6615-0410-a283-869df193e4dd

--- app/tpl/sru/userservice.php
+++ app/tpl/sru/userservice.php
@@ -15,14 +15,14 @@ extends UFtpl_Common {
 		foreach($d as $c){
 				echo '<li>';
 				echo date(self::TIME_YYMMDD_HHMM, $c['modifiedAt']);
-				echo ' poprosił o ';
+				echo '<small> poprosił o ';
 			if($c['type'] == 1){
 				echo 'aktywację';
 			}elseif($c['type'] == 3){ 
 				echo 'dezaktywację';
 			}
-			echo ' usługi: <a href="'.$url.'/services/">'.$this->_escape($c['servName']) . '</a> ';
-			echo ' <small>dla użytkownika: </small><a href="'.$url.'/users/'.$c['userId'].'">';
+			echo ' usługi: </small><a href="'.$url.'/services/">'.$this->_escape($c['servName']) . '</a> ';
+			echo ' <small> użytkownika: </small><a href="'.$url.'/users/'.$c['userId'].'">';
 			echo $this->_escape($c['userName']).' "'.$c['login'].'" '.$this->_escape($c['userSurname']).'</a>';
 			echo '</li>';
 		}
@@ -37,15 +37,15 @@ extends UFtpl_Common {
 		
 		foreach($d as $c){
 			echo '<li>';
-			echo date(self::TIME_YYMMDD_HHMM, $c['modifiedAt']);
+			echo date(self::TIME_YYMMDD_HHMM, $c['modifiedAt']) . '<small>';
 			if($c['type'] == 2){
 				echo ' aktywował';
 			}
 			elseif($c['type'] == 4){
 				echo ' dezaktywował';
 			}
-			echo ' usługę: <a href="'.$url.'/services/">'.$this->_escape($c['servName']) . '</a> ';
-			echo ' <small> użytkownikowi: </small><a href="'.$url.'/users/'.$c['userId'].'">'.$this->_escape($c['userName']).' "';
+			echo ' usługę: </small><a href="'.$url.'/services/">'.$this->_escape($c['servName']) . '</a> ';
+			echo ' <small> użytkownika: </small><a href="'.$url.'/users/'.$c['userId'].'">'.$this->_escape($c['userName']).' "';
 			echo $c['login'].'" '.$this->_escape($c['userSurname']).'</a>.';
 			echo '</li>';
 		}
