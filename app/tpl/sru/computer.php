3d0dab65c69ece4d0f5ed8861f4453bae487ded8
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-05-25 09:17:42 +0000

    #235 - podlinkowanie portu w widoku kompa
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@473 e0d11117-6615-0410-a283-869df193e4dd

--- app/tpl/sru/computer.php
+++ app/tpl/sru/computer.php
@@ -102,9 +102,9 @@ extends UFtpl_Common {
 		echo '<p><em>Właściciel:</em> '.$user.'</p>';
 		echo '<p><em>MAC:</em> ';
 		if ($switchPort != null) {
-			echo '<a href="'.$this->url(0).'/switches/'.$switchPort->switchId.'/port/'.$switchPort->id.'">'.$d['mac'].'</a> ';
+			echo '<a href="'.$this->url(0).'/switches/'.$switchPort->switchId.'/port/'.$switchPort->id.'/macs">'.$d['mac'].'</a> ';
 			echo '<small>(<a href="'.$this->url(0).'/switches/'.$switchPort->switchId.'">'.UFtpl_SruAdmin_Switch::displaySwitchName($switchPort->dormitoryAlias, $switchPort->switchNo).'</a>, ';
-			echo 'port '.$switchPort->ordinalNo.')</small>';
+			echo '<a href="'.$this->url(0).'/switches/'.$switchPort->switchId.'/port/'.$switchPort->id.'">port '.$switchPort->ordinalNo.'</a>)</small>';
 		} else {
 			echo $d['mac'];
 		}
