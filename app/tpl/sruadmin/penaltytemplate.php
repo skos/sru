a7d93bac32c1df936a73a77161fcd140bfb183c3
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-07-10 15:35:40 +0000

    #211 - [Moduł kar] Oddzielenie/pogrupowanie poszczególnych typów szablonów.
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@513 e0d11117-6615-0410-a283-869df193e4dd

--- app/tpl/sruadmin/penaltytemplate.php
+++ app/tpl/sruadmin/penaltytemplate.php
@@ -7,9 +7,15 @@ extends UFtpl_Common {
 
 	public function choose(array $d) {
 		$url = $this->url();
+		$lastTemplate = '-';
 		foreach ($d as $t) {
+			if ($lastTemplate != '-' && substr($t['title'], 0, 3) != substr($lastTemplate, 0, 3)) {
+				echo '<li><hr/></li>';
+			}
 			echo '<li><h3><a href="'.$url.'/template:'.$t['id'].'">'.$t['title'].'</a></h3><p>'.$t['description'].' <em>('.$t['duration'].' dni / '.$t['amnesty'].' dni)</em></p></li>';
+			$lastTemplate = $t['title'];
 		}
+		echo '<li><hr/></li>';
 		echo '<li><h3><a href="'.$this->url().'/template:0">Inne</a></h3><p>Żadne z powyższych</p></li>';
 	}
 
