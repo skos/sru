8bd611d349d8f04653264c4f57a3f01f5255642a
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-10-02 14:50:04 +0000

    #305 - [api] Wyrejestrowywanie kompów nie działa
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@593 e0d11117-6615-0410-a283-869df193e4dd

--- app/dao/sruadmin/admin.php
+++ app/dao/sruadmin/admin.php
@@ -81,7 +81,7 @@ extends UFdao {
 		$server = $this->_srv->get('req')->server;
 		$login = $server->PHP_AUTH_USER;
 		$password = $server->PHP_AUTH_PW;
-		$password = UFbean_Sru_User::generatePassword($password);
+		$password = UFbean_SruAdmin_Admin::generatePassword($login, $password);
 
 		return $this->getByLoginPassword($login, $password);
 	}
