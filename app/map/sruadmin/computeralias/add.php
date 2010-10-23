f5d4e42cbd1003113220c05125dc1b05aa30e40b
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-10-23 12:30:13 +0000

    #292 - [aliasy serwerów] Brak możliwości dodania aliasów www.coś
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@605 e0d11117-6615-0410-a283-869df193e4dd

--- app/map/sruadmin/computeralias/add.php
+++ app/map/sruadmin/computeralias/add.php
@@ -5,6 +5,6 @@
 class UFmap_SruAdmin_ComputerAlias_Add
 extends UFmap_SruAdmin_ComputerAlias_Get {
 	protected $valids = array(
-		'host' => array('textMin'=>1, 'textMax'=>50, 'regexp'=>'^[a-z][-a-z0-9]*$'),
+		'host' => array('textMin'=>1, 'textMax'=>50, 'regexp'=>'^[a-z][-a-z0-9]*$|^[a-z][-a-z0-9.]*[-a-z0-9]+$'),
 	);
 }
