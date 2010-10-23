036e8e2a5e0bcbbd7952fc52a6cb646d31cbeb6a
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-10-23 10:11:04 +0000

    #304 - [aliasy] Mogą zniknąć
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@600 e0d11117-6615-0410-a283-869df193e4dd

--- app/dao/sruadmin/computeralias.php
+++ app/dao/sruadmin/computeralias.php
@@ -23,4 +23,13 @@ extends UFdao {
 
 		return $this->doSelectFirst($query);
 	}
+
+	public function listAll($page=1, $perPage=10, $overFetch=0) {
+		$mapping = $this->mapping('list');
+
+		$query = $this->prepareSelect($mapping);
+		$query->order($mapping->host, $query->ASC);
+
+		return $this->doSelect($query);
+	}
 }
