d15aaff66fb7a10f557b74e04be7395d8ff91eeb
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2010-11-12 12:44:30 +0000

    #338 - [aliasy] nie działa dodawanie aliasów
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@616 e0d11117-6615-0410-a283-869df193e4dd

--- app/map/sruadmin/computeralias/add.php
+++ app/map/sruadmin/computeralias/add.php
@@ -3,7 +3,26 @@
  * dodanie aliasu komputera
  */
 class UFmap_SruAdmin_ComputerAlias_Add
-extends UFmap_SruAdmin_ComputerAlias_Get {
+extends UFmap {
+	protected $columns = array(
+		'id'		=> 'id',
+		'computerId'	=> 'computer_id',
+		'host'		=> 'host',
+		'isCname'	=> 'is_cname',
+	);
+
+	protected $columnTypes = array(
+		'id'		=> self::INT,
+		'computerId'	=> self::INT,
+		'host'		=> self::TEXT,
+		'isCname'	=> self::BOOL,
+	);
+	protected $tables = array(
+		'' => 'computers_aliases',
+	);
+
+	protected $pk = 'id';
+
 	protected $valids = array(
 		'host' => array('textMin'=>1, 'textMax'=>50, 'regexp'=>'^[a-z][-a-z0-9]*$|^[a-z][-a-z0-9.]*[-a-z0-9]+$'),
 	);
