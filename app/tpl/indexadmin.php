fbf4a54f7a3f6ce92e4e9656e265bbeb76e19aa1
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2011-03-20 14:10:41 +0000

    #412 - za dużo poszło ;)
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@777 e0d11117-6615-0410-a283-869df193e4dd

--- app/tpl/indexadmin.php
+++ app/tpl/indexadmin.php
@@ -15,7 +15,6 @@ extends UFtpl_Common {
 <link rel="stylesheet" href="<?=UFURL_BASE;?>/i/css/style.css" type="text/css" media="screen" />
 <link rel="shortcut icon" href="<?=UFURL_BASE;?>/i/img/favicon.ico" type="image/x-icon" />
 <script type="text/javascript" src="<?=UFURL_BASE;?>/i/jquery/jquery.tools.min.js"></script>
-<script type="text/javascript" src="<?=UFURL_BASE;?>/i/js/scripts.js"></script>
 <title><?=$d['title'];?></title>
 </head>
 <body>
