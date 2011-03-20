894c1d86025167895e908e57abaaa98bc39e7a5a
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2011-03-20 12:28:50 +0000

    #264 - mała poprawka
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@772 e0d11117-6615-0410-a283-869df193e4dd

--- app/sql/patch/r756.sql
+++ app/sql/patch/r756.sql
@@ -1,8 +1,8 @@
 ALTER TABLE computers ADD COLUMN last_seen timestamp without time zone;
 
-CREATE OR REPLACE FUNCTION computers_seen_update(inet, timestamp) returns boolean AS '
+CREATE OR REPLACE FUNCTION computers_seen_update(macaddr, timestamp) returns boolean AS '
 	BEGIN
-		UPDATE computers SET last_seen = $2 WHERE ipv4 = $1 and active = true;
+		UPDATE computers SET last_seen = $2 WHERE mac = $1 and active = true;
 		RETURN true;
 	END;
 ' LANGUAGE 'plpgsql' VOLATILE SECURITY DEFINER;
