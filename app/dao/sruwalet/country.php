4dde909a23c35cde0e22d444b225fb1ad8e8303a
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2011-09-18 18:15:47 +0000

    #498 poprawienie sortowania listy narodowości
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@994 e0d11117-6615-0410-a283-869df193e4dd

--- app/dao/sruwalet/country.php
+++ app/dao/sruwalet/country.php
@@ -40,4 +40,13 @@ extends UFdao {
 
 		return $this->doSelectFirst($query);
 	}
+
+	public function listAll($page=1, $perPage=10, $overFetch=0) {
+		$mapping = $this->mapping('list');
+
+		$query = $this->prepareSelect($mapping);
+		$query->order($mapping->nationality, $query->ASC);
+
+		return $this->doSelect($query);
+	}
 }
