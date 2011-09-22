98654d8772b785a669d9bd9b8869462ec33733d7
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2011-09-22 21:20:38 +0000

    Poprawione wielkości wykresów rrd
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@1022 e0d11117-6615-0410-a283-869df193e4dd

--- app/bin/rrd_graph.sh
+++ app/bin/rrd_graph.sh
@@ -13,35 +13,35 @@ function generate() {
 		'CDEF:bytesavgNorm=bytesavg,60,/' \
 		'AREA:bytesavgNorm#FFDDDD:average (10min)' \
 		'LINE1:bytesNorm#FF0000:transfer' \
-		-b 1024 -w 542 -h 200 -e "${3} ${4}" -s end-3h  -v B/s -t "${2} - 3h" -l 0
+		-b 1024 -w 863 -h 350 -e "${3} ${4}" -s end-3h  -v B/s -t "${2} - 3h" -l 0
 
 		echo -n "${1} day"
 		rrdtool graph "${dir}/${1}${3}${4}.day.png" \
 		"DEF:bytesavg=${data}/${1}.rrd:tcpbytes:AVERAGE" \
 		'CDEF:bytesavgNorm=bytesavg,60,/' \
 		'LINE1:bytesavgNorm#FF0000:transfer' \
-		-b 1024 -w 542 -h 200 -e "${3} ${4}" -s end-1d -v B/s -t "${2} - 24h" -l 0
+		-b 1024 -w 863 -h 350 -e "${3} ${4}" -s end-1d -v B/s -t "${2} - 24h" -l 0
 
                 echo -n "${1} week"
 		rrdtool graph "${dir}/${1}${3}${4}.week.png" \
 		"DEF:bytesavg=${data}/${1}.rrd:tcpbytes:AVERAGE" \
                 'CDEF:bytesavgNorm=bytesavg,60,/' \
                 'LINE1:bytesavgNorm#FF0000:transfer' \
-                -b 1024 -E -w 542 -h 200 -e "${3} ${4}" -s end-7d -v B/s -t "${2} - tydzień" -l 0
+                -b 1024 -E -w 863 -h 350-e "${3} ${4}" -s end-7d -v B/s -t "${2} - tydzień" -l 0
 
                 echo -n "${1} month"
 		rrdtool graph "${dir}/${1}${3}${4}.month.png" \
 		"DEF:bytesavg=${data}/${1}.rrd:tcpbytes:AVERAGE" \
                 'CDEF:bytesavgNorm=bytesavg,60,/' \
                 'AREA:bytesavgNorm#FF6666:transfer' \
-                -b 1024 -w 542 -h 200 -e "${3} ${4}" -s end-30d -v B/s -t "${2} - miesiąc" -l 0
+                -b 1024 -w 863 -h 350 -e "${3} ${4}" -s end-30d -v B/s -t "${2} - miesiąc" -l 0
                 
 		echo -n "${1} year"
 		rrdtool graph "${dir}/${1}${3}${4}.year.png" \
 		"DEF:bytesavg=${data}/${1}.rrd:tcpbytes:AVERAGE" \
                 'CDEF:bytesavgNorm=bytesavg,60,/' \
                 'AREA:bytesavgNorm#FF6666:transfer' \
-                -b 1024 -w 542 -h 200 -e "${3} ${4}" -s end-1y -v B/s -t "${2} - rok" -l 0
+                -b 1024 -w 863 -h 350 -e "${3} ${4}" -s end-1y -v B/s -t "${2} - rok" -l 0
 }
 
 if [[ -z ${1} || -z ${2} ]]; then
