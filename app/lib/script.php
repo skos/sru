ac8446758aaa4070b7906bc8e14b5a98c52c3615
mardraze <mardraze@e0d11117-6615-0410-a283-869df193e4dd>
2012-03-22 02:27:48 +0000

    Aktualizacja pliku ze skryptami
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@1124 e0d11117-6615-0410-a283-869df193e4dd

--- app/lib/script.php
+++ app/lib/script.php
@@ -7,6 +7,31 @@ class UFlib_Script {
             document.getElementById('<?php echo $id; ?>').focus();
         </script>
     <?}
+    
+    public static function SruAdmin_Switch_changeVisibility(){
+?><script type="text/javascript">
+function changeVisibility() {
+	var div = document.getElementById('switchMore');
+	if (div.sruHidden != true) {
+		div.style.display = 'none';
+		div.sruHidden = true;
+	} else {
+		div.style.display = 'block';
+		div.sruHidden = false;
+	}
+}
+var container = document.getElementById('switchMoreSwitch');
+var button = document.createElement('a');
+button.onclick = function() {
+	changeVisibility();
+}
+var txt = document.createTextNode('Szczegóły');
+button.appendChild(txt);
+container.appendChild(button);
+changeVisibility();
+</script><?
+        
+    }
 
 }
 ?>
