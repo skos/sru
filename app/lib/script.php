a36873a03f9ccbdbb4ff7d004bf9b6b80ca22ea9
mardraze <mardraze@e0d11117-6615-0410-a283-869df193e4dd>
2012-04-12 23:23:08 +0000

    Brakujaca metoda w skryptach
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@1146 e0d11117-6615-0410-a283-869df193e4dd

--- app/lib/script.php
+++ app/lib/script.php
@@ -1,69 +1,73 @@
 <?php
 /* Wszelkie skrypty JS */
 class UFlib_Script {
-    
-    public static function focus($id){?>        
-        <script type="text/javascript">
-            document.getElementById('<?php echo $id; ?>').focus();
-        </script>
-    <?}
-    
-    public static function switchMoreChangeVisibility(){
+
+	public static function focus($id){?>        
+	<script type="text/javascript">
+	    document.getElementById('<?php echo $id; ?>').focus();
+	</script>
+	<?}
+
+    public static function SruAdmin_Switch_changeVisibility(){
 ?><script type="text/javascript">
-function changeVisibility() {
-	var div = document.getElementById('switchMore');
-	if (div.sruHidden != true) {
-		div.style.display = 'none';
-		div.sruHidden = true;
-	} else {
-		div.style.display = 'block';
-		div.sruHidden = false;
+	function changeVisibility() {
+		var div = document.getElementById('switchMore');
+		if (div.sruHidden != true) {
+			div.style.display = 'none';
+			div.sruHidden = true;
+		} else {
+			div.style.display = 'block';
+			div.sruHidden = false;
+		}
 	}
-}
-var container = document.getElementById('switchMoreSwitch');
-var button = document.createElement('a');
-button.onclick = function() {
+	var container = document.getElementById('switchMoreSwitch');
+	var button = document.createElement('a');
+	button.onclick = function() {
+		changeVisibility();
+	}
+	var txt = document.createTextNode('Szczegóły');
+	button.appendChild(txt);
+	container.appendChild(button);
 	changeVisibility();
-}
-var txt = document.createTextNode('Szczegóły');
-button.appendChild(txt);
-container.appendChild(button);
-changeVisibility();
 </script><?
-        
+
     }
-	
-			
-		/**
-		 * Wyświetla menu kontekstowe edycji portu
-		 */
-		public static function displaySwitchPortMenu(array $switchUrls) {
-			echo '<ul id="switchContexMenu" class="contextMenu">
+
+                       
+    /**
+    * Wyświetla menu kontekstowe edycji portu
+    */
+public static function displaySwitchPortMenu(array $switchUrls) {
+                        echo '<ul id="switchContexMenu" class="contextMenu">
     <li class="editSwitchContexMenu">
         <a href="#edit">Edytuj</a>
     </li>
-	<li class="macSwitchContexMenu">
+        <li class="macSwitchContexMenu">
         <a href="#mac">Pokaż adresy MAC</a>
     </li>
 </ul>';
 
-			foreach ($switchUrls as $swId => $swUrl) {
+                        foreach ($switchUrls as $swId => $swUrl) {
 ?><script type="text/javascript">
 $(document).ready( function() {
     $("#switchPortsT<? echo $swId ?> td").contextMenu({
         menu: "switchContexMenu"
     },
         function(action, el, pos) {
-			if (action == "edit") {
-				window.location = "<? echo $swUrl ?>/port/" + $(el).attr("id") + "/:edit";
-			} else if (action == "mac") {
-				window.location = "<? echo $swUrl ?>/port/" + $(el).attr("id") + "/macs";
-			}
+                        if (action == "edit") {
+                                window.location = "<? echo $swUrl ?>/port/" + $(el).attr("id") + "/:edit";
+                        } else if (action == "mac") {
+                                window.location = "<? echo $swUrl ?>/port/" + $(el).attr("id") + "/macs";
+                        }
     });
 });
 </script><?
-			}
-		}
+                        }
+                }
+public static function switchmorechangevisibility() {
+    
+}
 
 }
+
 ?>
