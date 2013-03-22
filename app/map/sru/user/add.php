3fd29aab295bcfc383545eb6d058631f014a15db
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2013-03-22 22:28:23 +0000

    #704 Brak możliwości podwójnych imion i nazwisk
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@1238 e0d11117-6615-0410-a283-869df193e4dd

--- app/map/sru/user/add.php
+++ app/map/sru/user/add.php
@@ -82,8 +82,8 @@ extends UFmap {
 	protected $valids = array(
 		'login' => array('textMin'=>1, 'textMax'=>100, 'regexp'=>'^[-a-zA-Z0-9\.@_]+$'),
 		'password' => array('textMin'=>6),
-		'name' => array('textMin'=>1, 'textMax'=>100, 'regexp'=>'^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+$'),
-		'surname' => array('textMin'=>1, 'textMax'=>100, 'regexp'=>'^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+$'),
+		'name' => array('textMin'=>1, 'textMax'=>100, 'regexp'=>'^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]+$'),
+		'surname' => array('textMin'=>1, 'textMax'=>100, 'regexp'=>'^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]+$'),
 		'email' => array('email'=>true, 'textMax'=>100),
 		'gg' => array('regexp'=>'^(|[0-9]{3,50})$|[0]'),
 		'facultyId' => array('textMin'=>1, 'regexp'=>'^(1|2|3|4|5|6|7|8|9|0)$'),
