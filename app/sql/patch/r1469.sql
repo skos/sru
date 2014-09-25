b67df26b8d550c7154ff160faa390fb250e5f63c
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2014-09-25 17:53:01 +0000

    #852 Nie jest możliwe dodanie hosta
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@1473 e0d11117-6615-0410-a283-869df193e4dd

--- app/sql/patch/r1469.sql
+++ app/sql/patch/r1469.sql
@@ -1,6 +1,6 @@
 ALTER TABLE vlans ADD COLUMN domain_suffix character varying(20) NOT NULL DEFAULT 'ds.pg.gda.pl';
 
-ALTER TABLE computers ADD COLUMN domain_name character varying(70);
+ALTER TABLE computers ADD COLUMN domain_name character varying(70) DEFAULT '.ds.pg.gda.pl';
 
 UPDATE computers c SET domain_name = c.host || '.' || (SELECT domain_suffix FROM vlans v, ipv4s i WHERE v.id = i.vlan AND i.ip = c.ipv4);
 ALTER TABLE computers ALTER domain_name SET NOT NULL;
@@ -16,10 +16,14 @@ DROP INDEX computers_host_key;
 CREATE OR REPLACE FUNCTION computers_set_domain_name()
   RETURNS trigger AS
 $BODY$BEGIN
-IF
-	NEW.host!=OLD.host
-THEN
-	UPDATE computers c SET domain_name = c.host || '.' || (SELECT domain_suffix FROM vlans v, ipv4s i WHERE v.id = i.vlan AND i.ip = c.ipv4);
+IF ('INSERT' = TG_OP) THEN
+	UPDATE computers c SET domain_name = c.host || '.' || (SELECT domain_suffix FROM vlans v, ipv4s i WHERE v.id = i.vlan AND i.ip = c.ipv4) WHERE c.id = NEW.id;
+ELSIF ('UPDATE' = TG_OP) THEN
+	IF
+		NEW.host!=OLD.host
+	THEN
+		UPDATE computers c SET domain_name = c.host || '.' || (SELECT domain_suffix FROM vlans v, ipv4s i WHERE v.id = i.vlan AND i.ip = c.ipv4) WHERE c.id = OLD.id;
+	END IF;
 END IF;
 return NEW;
 END;$BODY$
