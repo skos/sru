2b7d87086c90d76ed3c1f98deefa4cb7a3f2f097
alan <alan@e0d11117-6615-0410-a283-869df193e4dd>
2014-11-13 14:19:36 +0000

    wysypalo sie na wdrozeniu ;)
    git-svn-id: http://svn.ds.pg.gda.pl/svn/sru/trunk@1492 e0d11117-6615-0410-a283-869df193e4dd

--- app/sql/patch/r1471.sql
+++ app/sql/patch/r1471.sql
@@ -1,3 +1,36 @@
+CREATE OR REPLACE FUNCTION device_update()
+  RETURNS trigger AS
+$BODY$BEGIN
+if
+        NEW.modified_by!=OLD.modified_by OR
+        NEW.modified_at!=OLD.modified_at OR
+        NEW.location_id!=OLD.location_id OR
+        NEW.device_model_id!=OLD.device_model_id OR
+        NEW.comment!=OLD.comment OR
+        (OLD.comment IS NOT NULL AND NEW.comment IS NULL) OR
+        (OLD.comment IS NULL AND NEW.comment IS NOT NULL)
+then
+        INSERT INTO devices_history (
+                device_id,
+                modified_by,
+                modified_at,
+                location_id,
+                device_model_id,
+                comment
+        ) VALUES (
+                OLD.id,
+                OLD.modified_by,
+                OLD.modified_at,
+                OLD.location_id,
+                OLD.device_model_id,
+                OLD.comment
+        );
+end if;
+return NEW;
+END;$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+
 ALTER TABLE devices RENAME COLUMN inoperational TO used;
 ALTER TABLE devices_history RENAME COLUMN inoperational TO used;
 UPDATE devices SET used = NOT used;
