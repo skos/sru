6a866ef537c49c14f85442257847b14a4f4cc046
alan <alan@ds.pg.gda.pl>
2016-01-17 15:16:38 +0100

    SRU-928 Umożliwienie SSPG opiniowania złożonego wniosku SRU-929 Umożliwienie adminowi opiniowania złożonego wniosku

--- app/tpl/sru/fwexceptionapplication.php
+++ app/tpl/sru/fwexceptionapplication.php
@@ -81,7 +81,7 @@ $(document).ready(function()
 		echo '<p><em>Komentarz:</em> '.$d['comment'].'</p>';
 		echo '<p><em>Edukacja własna:</em> '.($d['selfEducation'] ? 'tak' : 'nie').'</p>';
 		echo '<p><em>Edukacja PG:</em> '.($d['universityEducation'] ? 'tak' : 'nie').'</p>';
-		echo '<p><em>Opinia SKOS:</em> '. $d['skosOpinion'].'</p>';
+		echo '<p><em>Opinia SKOS:</em> '. $d['skosComment'].'</p>';
 		echo $form->sspgComment('Komentarz', array('type'=>$form->TEXTAREA, 'rows'=>2));
 		$tmp = array(
 				'0' => 'Nie',
